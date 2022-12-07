725b2887940f9f019dc6326edbce3dd4f9acc740
Chris Fison <47662472+chris-fison@users.noreply.github.com>
2022-12-07 19:32:49 +0000

    Create uks-data.tf

---
+++ uks-data.tf
@@ -0,0 +1,6 @@
+variable "managed_image_name" {
+   description = "Name of the managed image"
+}
+variable "managed_image_resource_group_name" {
+   description = "Name of the managed image resource group"
+}
